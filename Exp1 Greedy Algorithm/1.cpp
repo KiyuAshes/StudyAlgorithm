2a45840c8a254895b342b7873714ed91d22d4ac0
KiyuAshes <kiyuashes@gmail.com>
2023-04-16 19:11:18 +0000

    Exp1 1.cpp 优化模拟解法

--- Exp1 Greedy Algorithm/1.cpp
+++ Exp1 Greedy Algorithm/1.cpp
@@ -1,14 +1,11 @@
 /*
  * @Author: KiyuAshes
  * @Date: 2023-04-16 18:21:27
- * @LastEditTime: 2023-04-16 18:32:30
+ * @LastEditTime: 2023-04-16 19:11:01
  * @Description: USC OJ 1388 集邮票
  * @E-mail: kiyuashes@stu.usc.edu.cn
  * Copyright (c) 2023 by KiyuAshes, All Rights Reserved. 
  */
-
-
-
 
 #include <algorithm>
 #include <climits>
@@ -21,41 +18,56 @@ typedef struct stamp {
 } stamp;
 
 int main() {
-  int n, m, total = INT_MAX, p = 0;
+  long n, m;
+  int i = 0, step, min = INT_MAX, total = INT_MAX;
   stamp stp[100050];
 
-  scanf("%d%d", &n, &m);
+  scanf("%ld%ld", &n, &m);
 
-  for (int i = 0; i < n; i++) {
+  for (i = 0; i < n; i++) {
     scanf("%d", &stp[i].num);
     if (stp[i].num < total)
       total = stp[i].num;
   }
 
-  for (int i = 0; i < n; i++) {
+  for (i = 0; i < n; i++) {
     scanf("%d", &stp[i].max);
   }
 
   sort(stp, stp + n, [](stamp x, stamp y) -> bool { return x.num < y.num; });
 
-
+  i = 0;
   while (m > 0) {
-    if (stp[p].num > total) {
-      total++;
-      p = 0;
+    if (stp[i].num > total) {
+
+      step = stp[i].num - total;
+
+      if (step >= min || step >= m / i) {
+        total += min < (m / i) ? min : (m / i);
+        break;
+      }
+
+      total += step;
+
+      i = 0;
+      min = INT_MAX;
       continue;
     }
 
-    if (stp[p].max == 0)
-      break;
+    if (stp[i].num < total) {
+      step = total - stp[i].num;
 
-    stp[p].max--;
-    stp[p].num++;
+      stp[i].max -= step;
+      stp[i].num += step;
 
-    p++;
-    m--;
+      m -= step;
+    }
+
+    if (stp[i].max < min)
+      min = stp[i].max;
+
+    i++;
   }
-
 
   printf("%d ", total);
 
